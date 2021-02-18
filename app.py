12b3a18e2630cdbdaeecd13bc6d4bb1f0bf8d4b9
Dyaa Ahmed <dyaaeldinahmed@gmail.com>
2021-02-18 21:50:24 +0000

    app.py edited online with Bitbucket

--- app.py
+++ app.py
@@ -10,18 +10,17 @@ DB_USER = os.environ[MYSQL_USER]
 DB_PASSWORD = os.environ[MYSQL_PASSWORD]
 DB_NAME = os.environ[MYSQL_DBNAME]
 
-con = _mysql.connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)        
-con.query("SELECT VERSION()")
-result = con.use_result()
-version = result.fetch_row()[0]
-
-
+mydb = mysql.connector.connect(
+  host=DB_HOST,
+  user=DB_USER,
+  password=DB_PASSWORD
+)
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return version
+    return mydb
 
 if __name__ == "__main__":
     app.run()
