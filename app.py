347d27acf939b20057082dadc78215dfb70a11fa
Dyaa Ahmed <dyaaeldinahmed@gmail.com>
2021-02-18 20:21:14 +0000

    app.py created online with Bitbucket

---
+++ app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import sys
+import _mysql
+from flask import Flask
+import MySQLdb as mdb
+
+DB_HOST = os.environ[MYSQL_HOST]
+DB_USER = os.environ[MYSQL_USER]
+DB_PASSWORD = os.environ[MYSQL_PASSWORD]
+DB_NAME = os.environ[MYSQL_DBNAME]
+
+con = _mysql.connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)        
+con.query("SELECT VERSION()")
+result = con.use_result()
+version = result.fetch_row()[0]
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return version
